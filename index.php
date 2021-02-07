54b0060a6d857958d33622b41cd42f60e57573ab
iamaks93 <mr.mahajan@hotmail.com>
2021-02-07 10:34:42 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
-<?php 
-
-echo "Hello world";
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset=utf-8>
+  <title>First release</title>
+</head>
+<body>
+  <h1>Hello Nikita</h1>
+</body>
+</html>
